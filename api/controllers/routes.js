50c47c353d89c0215fb96a35f3a6b76282215c89
rileymiller@mymail.mines.edu <riley.miller@regiscompany.com>
2018-06-02 11:36:16 -0600

    working on creating render routes

--- api/controllers/routes.js
+++ api/controllers/routes.js
@@ -1,13 +1,58 @@
 // Route GET and POST functions
 
+var request = require('request');
+var apiOptions = {
+    server: "http://localhost:8080"
+};
+
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://nameless-basin-42853.herokuapp.com";
+}
+
 /** 
  * GET /viewteamgroup 
-*/
+ */
 exports.getViewgroupteam = (req, res) => {
-  res.render('viewgroupteam.pug', {
-    title: 'View Group/Team'
-  });
+    console.log('inside getViewgroupteam in routes.js');
+    
+
+    getTeams(req, res, function(req, res, responseData) {
+        renderViewgroupteam(req, res, responseData);
+    });
 };
+
+var getTeams = (req, res, callback) => {
+    var requestOptions, path;
+    console.log('inside getTeams');
+    path = "/api/allTeams";
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {}
+    };
+    request(
+        requestOptions,
+        function(err, response, body) {
+            var data = body;
+            if (response.statusCode === 200) {
+
+                callback(req, res, data);
+            } else {
+                _showError(req, res, response.statusCode);
+            }
+        }
+    );
+};
+
+var renderViewgroupteam = (req, res, responseData) => {
+    console.log('inside renderViewgroupteam');
+
+
+    res.render('viewgroupteam.pug', {
+        title: 'View Group/Team',
+        teams: responseData
+    });
+}
 
 /**
  * POST /viewgroupteam.
@@ -15,4 +60,3 @@ exports.getViewgroupteam = (req, res) => {
 exports.postViewgroupteam = (req, res, next) => {
     res.redirect('/viewgroupteam.pug');
 };
-
