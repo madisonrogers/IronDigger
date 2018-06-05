ce9b979618ba68d2d78f07015b50608f72fab0b0
Madison Rogers <madisonrogers@csm-wl-dhcp-231-101.mines.edu>
2018-06-05 17:09:14 -0600

    Working on adding exercise and block to the create phase modal

--- public/js/createPhaseCtrl.js
+++ public/js/createPhaseCtrl.js
@@ -1,4 +1,6 @@
 var last_selected_time;
+var cloneCount = 1;
+var blockCount = 1;
 
 $(function() {
 
@@ -10,19 +12,18 @@ $(function() {
 	  	height: 'auto',
 	  	contentHeight: 'auto',
 	  	dayClick: function(date, jsEvent, view) {
-			console.log('Clicked on: ' + date.format());
-			console.log(last_selected_time);
-			console.log(date);
+			// console.log('Clicked on: ' + date.format());
+			// console.log(last_selected_time);
+			// console.log(date);
 			last_selected_time = date;
 		
 			$('#workoutDate').val(date.format('l'));
 			$('#workoutModal').modal('toggle');
 			
 		},
-		select: function(startDate, endDate) {
-			// This is where the modal will be created to create the phase
-	      	console.log('selected ' + startDate.format() + ' to ' + endDate.format());
-	    },
+		// select: function(startDate, endDate) {
+		// 	// This is where the modal will be created to create the phase
+	 //    },
 	    // we will be able to load in workouts from the DB
 	    events: [
 		    {
@@ -40,66 +41,65 @@ $(function() {
 
 
 	$("[id^=add-block-").click(function() {
-		var block_button = $(this).attr("id"); 
+		var num = $(this).attr("id"); 
+		var block_button = num.substr(num.lastIndexOf("-")+1)
 		console.log(block_button)
-		var block = block_button.substring(block_button.indexOf('-') +1);
-		console.log(block)
-
+		$( ".block-1-ex-1" ).clone(true).attr('class', 'block-'+(blockCount)+'-ex-'+(++cloneCount)).insertAfter('.block-1-ex-'+(cloneCount-1));
 	});
 
-		var $TABLE = $('#table');
-		var $BTN = $('#export-btn');
-		var $EXPORT = $('#export');
+	var $TABLE = $('#table');
+	var $BTN = $('#export-btn');
+	var $EXPORT = $('#export');
 
-		$('.table-add').click(function () {
-			var $clone = $TABLE.find('tr.clone').clone(true).removeClass('clone table-line');
-			$TABLE.find('table').append($clone);
+	$('.table-add').click(function () {
+		var $clone = $TABLE.find('tr.clone').clone(true).removeClass('clone table-line');
+		$TABLE.find('table').append($clone);
+	});
+
+	$('.table-remove').click(function () {
+		$(this).parents('tr').detach();
+	});
+
+	$('.table-up').click(function () {
+		var $row = $(this).parents('tr');
+		if ($row.index() === 1) return; // Don't go above the header
+		$row.prev().before($row.get(0));
+	});
+
+	$('.table-down').click(function () {
+		var $row = $(this).parents('tr');
+		$row.next().after($row.get(0));
+	});
+
+	// A few jQuery helpers for exporting only
+	jQuery.fn.pop = [].pop;
+	jQuery.fn.shift = [].shift;
+
+	$BTN.click(function () {
+		var $rows = $TABLE.find('tr:not(:hidden)');
+		var headers = [];
+		var data = [];
+		
+		// Get the headers (add special header logic here)
+		$($rows.shift()).find('th:not(:empty)').each(function () {
+			headers.push($(this).text().toLowerCase());
 		});
-
-		$('.table-remove').click(function () {
-			$(this).parents('tr').detach();
-		});
-
-		$('.table-up').click(function () {
-			var $row = $(this).parents('tr');
-			if ($row.index() === 1) return; // Don't go above the header
-			$row.prev().before($row.get(0));
-		});
-
-		$('.table-down').click(function () {
-			var $row = $(this).parents('tr');
-			$row.next().after($row.get(0));
-		});
-
-		// A few jQuery helpers for exporting only
-		jQuery.fn.pop = [].pop;
-		jQuery.fn.shift = [].shift;
-
-		$BTN.click(function () {
-			var $rows = $TABLE.find('tr:not(:hidden)');
-			var headers = [];
-			var data = [];
+		
+		// Turn all existing rows into a loopable array
+		$rows.each(function () {
+			var $td = $(this).find('td');
+			var h = {};
 			
-			// Get the headers (add special header logic here)
-			$($rows.shift()).find('th:not(:empty)').each(function () {
-				headers.push($(this).text().toLowerCase());
+			// Use the headers from earlier to name our hash keys
+			headers.forEach(function (header, i) {
+				h[header] = $td.eq(i).text();   
 			});
 			
-			// Turn all existing rows into a loopable array
-			$rows.each(function () {
-				var $td = $(this).find('td');
-				var h = {};
-				
-				// Use the headers from earlier to name our hash keys
-				headers.forEach(function (header, i) {
-					h[header] = $td.eq(i).text();   
-				});
-				
-				data.push(h);
-			});
-			
-			// Output the result
-			$EXPORT.text(JSON.stringify(data));
+			data.push(h);
 		});
+		
+		// Output the result
+		$EXPORT.text(JSON.stringify(data));
+	});
 
 });
