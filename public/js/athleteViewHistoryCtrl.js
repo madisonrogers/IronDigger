106a4c6b7ec997561d3c27a0c3f63dc06bde4405
Madison Rogers <madisonrogers@csm-wl-dhcp-228-13.mines.edu>
2018-06-16 12:14:48 -0600

    Finished the athlete history page. The table and graph now show up for the selected exercise

--- public/js/athleteViewHistoryCtrl.js
+++ public/js/athleteViewHistoryCtrl.js
@@ -6,6 +6,7 @@ var selectedExercise;
 var xData;
 var yData;
 var repData = [];
+var setData = [];
 var graphable = true;
 var earliestDate;
 var latestDate;
@@ -53,6 +54,7 @@ const parseData = () => {
 		} else {
 			yData.push(set.actweight);
 			repData.push(set.reps);
+			setData.push(set.sets);
 			var dateStr = set.date;
 			var month = parseInt(dateStr.substr(0,dateStr.indexOf('/')));
 			dateStr = dateStr.substr(dateStr.indexOf('/') + 1);
@@ -84,14 +86,17 @@ const parseData = () => {
 const createGraph = () => {
 
 	if (graphable) {
+		// add title
 		console.log('graphing')
 		var h = window.innerHeight*0.5;
-		var w = window.innerWidth*0.95;
+		// var w = window.innerWidth*0.95;
+		var w = $('#graph').width();
+		console.log(w)
 		var padding = 30;
 
 
 		var dataset = xData.map(function(e, i) {
-		  return [e, yData[i], repData[i]];
+		  return [e, yData[i], repData[i], setData[i]];
 		});
 
 		console.log(dataset)
@@ -104,7 +109,7 @@ const createGraph = () => {
 		latestDate = new Date(latestDate.getFullYear(), latestDate.getMonth(), latestDate.getDate()+1)
 		var xScale = d3.time.scale()
             .domain([earliestDate, latestDate])
-            .range([padding, w - padding*2]);
+            .range([padding, w - padding*3]);
 
         var yScale = d3.scale.linear()
             .domain([0, d3.max(dataset, function(d) { return d[1]; })])
@@ -117,8 +122,9 @@ const createGraph = () => {
 
 		var svg = d3.select("#graph")
 		  .append("svg")
-		  .attr("width", w)
-		  .attr("height", h);
+		  .attr("width", w-2*padding)
+		  .attr("height", h+padding)
+		  .style("padding-left", padding);
 
 		svg.selectAll("circle")
 		  .data(dataset)
@@ -133,16 +139,16 @@ const createGraph = () => {
 		  .attr("r", function(d) {
 		    return 5;
 		  })
-		  .attr("fill", "#00aa88")
+		  .attr("fill", "grey")
 		  .on("click", function(d) {		
 	            div.transition()		
 	                .duration(200)		
 	                .style("opacity", .9);		
-	            div.html('Date: '+d[0].getMonth()+'/'+d[0].getDate()+'/'+d[0].getFullYear() + "<br/>Weight: "  + d[1]+ "<br/>Reps: " + d[2])
+	            div.html('Date: '+d[0].getMonth()+'/'+d[0].getDate()+'/'+d[0].getFullYear() + "<br/>Set: "+d[3]+"<br/>Reps: " + d[2] + "<br/>Weight: "  + d[1])
 	                .style("left", (d3.event.pageX) + "px")		
 	                .style("top", (d3.event.pageY - 28) + "px");	
 	            })					
-	        .on("losefocus", function(d) {		
+	        .on("focusout", function(d) {		
 	            div.transition()		
 	                .duration(500)		
 	                .style("opacity", 0);	
@@ -167,10 +173,52 @@ const createGraph = () => {
 		    .attr("class", "axis")
 		    .attr("transform", "translate(" + padding + ",0)")
 		    .call(yAxis);
+
+		 // Add the text label for the x axis
+	    svg.append("text")
+	        .attr("transform", "translate(" + (w / 2) + " ," + (h+padding) + ")")
+	        .style("text-anchor", "middle")
+	        .text("Date");
+
+	    // Add the text label for the Y axis
+	    svg.append("text")
+	        .attr("transform", "rotate(-90)")
+	        .attr("y", 0-padding)
+	        .attr("x",0 - (h / 2))
+	        .attr("dy", "1em")
+	        .style("text-anchor", "middle")
+	        .text("Weight");
+
+	    createTable();
+
 	} else {
 		$("#graph").append("<div class='alert alert-primary' role='alert'>Exercise not graphable</div>")
 	}
 	
+}
+
+const createTable = () => {
+	// create a th for each distinct date
+	for(var i = 0; i < uniqueDates.length; i++) {
+		$('#table').append("<thead id="+i+"><tr><th>Date: "+uniqueDates[i].getMonth()+"/"+uniqueDates[i].getDate()+"/"+uniqueDates[i].getFullYear()+"</th></tr></thead>")
+
+		// loop through all the exercises and make rows for the ones with the matching dates
+		$('#'+i).append("<tr id='header'><td>Set</td><td>Rep</td><td>Weight</td></tr>")
+		$('#'+i).append("<tbody>");
+		for(var j = 0; j < selectedExercise.sets.length; j++) {
+			var dateStr = selectedExercise.sets[j].date;
+			var month = parseInt(dateStr.substr(0,dateStr.indexOf('/')));
+			dateStr = dateStr.substr(dateStr.indexOf('/') + 1);
+			var day = parseInt(dateStr.substr(0,dateStr.indexOf('/')));
+			dateStr = dateStr.substr(dateStr.indexOf('/') + 1);
+			var year = parseInt(dateStr);
+			if(month == uniqueDates[i].getMonth() && day == uniqueDates[i].getDate() && year == uniqueDates[i].getFullYear()) {
+				// same date
+				$('#table').append("<tr><td>"+selectedExercise.sets[j].sets+"</td><td>"+selectedExercise.sets[j].reps+"</td><td>"+selectedExercise.sets[j].actweight+"</td></tr>");
+			}
+		}
+	}
+	$('#table').append('</tbody>');
 }
 
 $(function() {
@@ -180,6 +228,7 @@ $(function() {
 		// remove alert if it exists
 		$('.alert').detach();
 		$('#graph').empty();
+		$('#table').empty();
 		selectedExerciseId = this.value;
 		// get the selected exercise obj
 		exercises.forEach(function(exercise) {
