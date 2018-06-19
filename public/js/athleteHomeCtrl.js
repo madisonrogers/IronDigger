d7475d41f7556470ac273f09eaec7e65c69d0a89
Madison Rogers <madisonrogers@MacBook-Pro-3.local>
2018-06-18 22:39:52 -0600

    styling for display workout complete

--- public/js/athleteHomeCtrl.js
+++ public/js/athleteHomeCtrl.js
@@ -29,26 +29,28 @@ const getPhases = () => {
 
 const populateWorkouts = () => {
   for(var i = 0; i < selectedPhase.workouts.length; i++) {
-    $('#w').append('<p id=workout'+i+'>'+selectedPhase.workouts[i].name+'</p>');
+    $('#w').append('<div id=workout'+i+'>'+selectedPhase.workouts[i].name+'<br/>Date: '+selectedPhase.workouts[i].date+'<i class="float-right fas fa-angle-down"></i></dib>');
 
     // populate the blocks 
     var blocks = selectedPhase.workouts[i].blocks
     for(var j = 0; j < blocks.length; j++) {
-      $('#workout'+i).attr('onclick', 'showBlocks('+i+','+j+',event)').append('<div id=block'+i+''+j+' style="display: none;">'+blocks[j].name+"</div>");
+      $('#workout'+i).attr('onclick', 'showBlocks('+i+','+j+',event)').append('<div id=block'+i+''+j+' style="display: none;">'+blocks[j].name+'<i class="float-right fas fa-angle-down"></i></div>');
 
       // populate the exercises
       var exercises = blocks[j].exercises;
+      console.log(exercises);
       for(var z = 0; z < exercises.length; z++) {
-        $('#block'+i+''+j).attr('onclick', 'showExercises('+i+','+j+','+z+',event)').append('<div id=exercise'+i+''+j+''+z+' style="display: none;">'+exercises[z].name+'</div>');
+        $('#block'+i+''+j).attr('onclick', 'showExercises('+i+','+j+','+z+',event)').append('<div id="exercise'+i+''+j+''+z+'" style="display: none;">'+exercises[z].name+'<i class="float-right fas fa-angle-down"></i></div>');
 
         // populate the sets, reps, and percentage in an editable table
         var sets = exercises[z].sets;
         $('#exercise'+i+''+j+''+z).attr('onclick', 'showSets('+i+','+j+','+z+',event)').append("<table class='table' id='liftingTable"+i+j+z+"' style='display: none;'></table>")
         $('#liftingTable'+i+j+z).append("<tr id='header"+i+''+j+''+z+"'><td>Set</td><td>Rep</td><td>Percentage</td><td>Actual Weight</td></tr>")
         for(var s = 0; s < sets.length; s++) {
-          $('#liftingTable'+i+j+z).attr('onclick', 'showTable(event)').append("<tr id='set"+i+''+j+''+z+''+s+"'><td>"+sets[s].set+"</td><td>"+sets[s].reps+"</td><td>"+sets[s].percent+"</td><td contenteditable='true'></td></tr>");
+          $('#liftingTable'+i+j+z).attr('onclick', 'showTable(event)').append("<tr id='set"+i+''+j+''+z+''+s+"'><td>"+sets[s].set+"</td><td>"+sets[s].reps+"</td><td>"+sets[s].percent+"</td><td id='actualWeight' contenteditable='true'></td></tr>");
         }
       }
+      $('#workout'+i).append("<button id='button"+i+"' type='button' class='btn btn-secondary btn-sm'>Submit Workout</button>")
     }
   }
 }
@@ -56,6 +58,7 @@ const populateWorkouts = () => {
 const showBlocks = (i,j,event) => {
   if($('#block'+i+j).css('display') == 'none') {
     $('#block'+i+j).css('display','block');
+    $('#block'+i+j).siblings().css('display', 'block');
   } else {
     $('#block'+i+j).css('display','none');
   }
@@ -65,8 +68,10 @@ const showBlocks = (i,j,event) => {
 const showExercises = (i,j,z,event) => {
   if($('#exercise'+i+j+z).css('display') == 'none') {
     $('#exercise'+i+j+z).css('display', 'block');
+    $('#exercise'+i+j+z).siblings().css('display', 'block');
   } else {
     $('#exercise'+i+j+z).css('display', 'none');
+    $('#exercise'+i+j+z).siblings().css('display', 'none');
   }
   event.stopPropagation();
 }
